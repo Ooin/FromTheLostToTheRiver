4a1aab25d2b061e4698d4550d2e39ed6a2e036b0
Marcos <marcosmejiasg@gmail.com>
2015-08-18 16:31:28 +0200

    Create test TransportDAOImpl (MEJOR QUE EL TUYO AITOR) and Implement to the Rafter Entity.

--- test/com/river/persistence/dao/implementation/TransportDAOImplTest.java
+++ test/com/river/persistence/dao/implementation/TransportDAOImplTest.java
@@ -3,6 +3,7 @@ package com.river.persistence.dao.implementation;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,43 +11,63 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.river.entity.Transport;
-import com.river.persistence.dao.implementation.TransportDAOImpl;
 import com.river.persistence.dao.interfaces.TransportDAO;
 
 public class TransportDAOImplTest {
 
-	Transport transport;
 	List<Transport> transports;
 	ApplicationContext context;
-	TransportDAO dao;
+	TransportDAO transportDAO;
+	Transport transportDB;
 
 	@Before
 	public void initTest() {
-		transport = new Transport("Linea 2 metro");
 		context = new ClassPathXmlApplicationContext("config/spring-config.xml");
-		dao = context.getBean(TransportDAOImpl.class);
+		transportDAO = context.getBean(TransportDAOImpl.class);
 		transports = new ArrayList<>();
+		transportDB = transportDAO.create(new Transport("Linea 5 metro"));
 	}
 
 	@Test
 	public void createTransportIfCorrect() {
-		transport = dao.create(transport);
+		Transport transport = transportDAO.create(new Transport("Linea 2 metro"));
 
 		Assert.assertNotNull("The returned value must be not null when creating an object", transport);
 		Assert.assertNotNull("The returned value must have a valid id when creating an object", transport.getId());
+		transportDAO.delete(transport);
 	}
 
 	@Test
 	public void createTransportIfexist() {
-		Transport creteTransport = dao.read(transport);
+		Transport transport = new Transport("Linea 2 metro");
+		transportDAO.create(transport);
+		Transport toAdd = new Transport("Linea 2 metro");
 
-		Assert.assertNull("The returned value must be not null when creating an object", creteTransport);
-		Assert.assertNull("The returned value must have a valid id when creating an object", creteTransport.getId());
+		toAdd = transportDAO.create(toAdd);
+		Assert.assertNull("The returned value must be null when no creating an object", toAdd);
+		transportDAO.delete(transport);
 	}
 
 	@Test
-	public void readTransportBeNull() {
+	public void readTransportIfExistNotNull() {
+		Transport toAdd = transportDAO.read(transportDB);
+		Assert.assertNotNull("The Transport exist on the data base.", toAdd);
+	}
 
+	@Test
+	public void readTransportNonBeNull() {
+		Transport toAdd = transportDAO.read(new Transport());
+		Assert.assertNull("The Transport don't exist on the data base.", toAdd);
+	}
+	
+	
+	
+	
+	
+	
+	@After
+	public void afterTest() {
+		transportDAO.delete(transportDB);
 	}
 
 }

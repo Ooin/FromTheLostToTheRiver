b3cbed33c3f44c5890ca8a589f9f65a26e9d5b98
arvd <arvd@PCBCN1084.gft.com>
2015-08-18 16:47:38 +0200

    rafter test done

--- test/com/river/persistence/dao/implementation/RafterDAOImplTest.java
+++ test/com/river/persistence/dao/implementation/RafterDAOImplTest.java
@@ -30,7 +30,6 @@ public class RafterDAOImplTest {
 	 public void addANonExistingRafter(){
 		 Rafter test = rafterDAO.create(new Rafter("capullo","12354", "testRaft3er@gmail.com", "9999-99-99"));
 		 Assert.assertNotNull("it should returns not null", test);
-		 System.out.println(test);
 		 rafterDAO.delete(test);
 		 
 	 }
@@ -45,10 +44,7 @@ public class RafterDAOImplTest {
 		Assert.assertNull(toAdd);
 		rafterDAO.delete(rafter);
 		
-		 
 	 }
-	 
-	 
 	 
 	 //add an empty rafter
 	@Test
@@ -58,31 +54,84 @@ public class RafterDAOImplTest {
 	 }
 	 
 	 //add a null rafter
-	 
+	@Test
+	 public void addANullRafter(){
+		 Rafter test = rafterDAO.create(new Rafter(null, null, null, null));
+		 Assert.assertNull("it should returns null", test);
+	 }
 	 
 	//#####READ TESTS########
 	 
 		 //read a non existing rafter
+		@Test
+		public void readANonExistingRafter(){
+			Rafter rafter = new Rafter(1234123);
+			rafter = rafterDAO.read(rafter);
+			Assert.assertNull("it should returns null", rafter);
+		}
 		 
 		 //read an existing rafter
-		 
+		@Test
+		public void readAnExistingRafter(){
+			Rafter rafter = new Rafter("testRafter5","12345", "testRafter5@gmail.com", "9999-99-99");
+			rafterDAO.create(rafter);
+			rafter = rafterDAO.read(rafter);
+			Assert.assertNotNull("it should returns not null", rafter);
+			rafterDAO.delete(rafter);
+		}
 		//#####UPDATE TESTS########
 	 
 	//#####UPDATE TESTS########
 	 //update an existing rafter
+	 @Test
+	 public void updateAnExistingRafter(){
+		 Rafter rafter = new Rafter("testRafter","1234", "testRafter@gmail.com", "9999-99-99");
+		 rafterDAO.create(rafter);
+		 rafter.setEmail("capullo");
+		 rafterDAO.update(rafter);
+		 Assert.assertNotNull("it should returns not null", rafter);
+		 Assert.assertEquals("email should be capullo", rafter.getEmail(), "capullo");
+		 rafterDAO.delete(rafter);
+		 
+	 }
 	 
 	 //update an empty rafter
+	 @Test
+	 public void updateAnEmptyRafter(){
+		Rafter rafter = new Rafter();
+		rafter = rafterDAO.update(rafter);
+		Assert.assertNull(rafter);
+		 
+	 }
 	 
-	 //update a null rafter
 	 
 	 //update a non existing rafter
-	 
+	 @Test
+	 public void updateANonExistingRafter(){
+		 Rafter rafter = new Rafter(888988);
+		 rafter = rafterDAO.update(rafter);
+		 Assert.assertNull("it should returns null", rafter);
+	 }
 	//#####DELETE TESTS########
 	 
 		 //delete an existing rafter
-		 
+	@Test
+	public void deleteAnExistingRafter(){
+		Rafter rafter = new Rafter("testRafter","1234", "testRafter@gmail.com", "9999-99-99");
+		rafterDAO.create(rafter);
+		rafter = rafterDAO.delete(rafter);
+		Assert.assertNotNull("it should returns a not null object", rafter);
+	}
 		 //delete a non existing rafter
+	@Test
+	public void deleteANonExistingRafter(){
+		Rafter rafter = new Rafter("testRafter","1234", "testRafter@gmail.com", "9999-99-99");
+		rafterDAO.create(rafter);
+		rafter.setId(234234);
+		rafter = rafterDAO.delete(rafter);
+		Assert.assertNull("it should returns a null object", rafter);
+	}
 	 	
-	 	//delete a rafter with dependencies
+	//TODO delete a rafter with dependencies
  
 }

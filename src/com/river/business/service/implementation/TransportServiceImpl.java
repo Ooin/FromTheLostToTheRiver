7334f7411e41fa60d7788480d7449f401f84bde9
Marcos <marcosmejiasg@gmail.com>
2015-08-18 12:16:18 +0200

    Esto funciona bien.

--- src/com/river/business/service/implementation/TransportServiceImpl.java
+++ src/com/river/business/service/implementation/TransportServiceImpl.java
@@ -4,42 +4,43 @@ import java.util.List;
 
 import com.river.business.service.interfaces.TransportService;
 import com.river.entity.Transport;
+import com.river.persistence.dao.interfaces.TransportDAO;
 
-public class TransportServiceImpl implements TransportService{
+public class TransportServiceImpl implements TransportService {
 
-	Transport transportDAO;
-	
-	public Transport getTransportDAO() {
+	TransportDAO transportDAO;
+
+	public TransportDAO getTransportDAO() {
 		return transportDAO;
 	}
 
-	public void setTransportDAO(Transport transportDAO) {
+	public void setTransportDAO(TransportDAO transportDAO) {
 		this.transportDAO = transportDAO;
 	}
 
 	@Override
 	public Transport create(Transport toCreate) {
-		return null;
+		return transportDAO.create(toCreate);
 	}
 
 	@Override
 	public Transport read(Transport toRead) {
-		return null;
+		return transportDAO.read(toRead);
 	}
 
 	@Override
 	public List<Transport> read() {
-		return null;
+		return transportDAO.read();
 	}
 
 	@Override
 	public Transport update(Transport toUpdate) {
-		return null;
+		return transportDAO.update(toUpdate);
 	}
 
 	@Override
 	public Transport delete(Transport toDelete) {
-		return null;
+		return transportDAO.delete(toDelete);
 	}
 
 }

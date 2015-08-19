7930d2efadd08d7dbcbd1ce64f46c55f6a24af52
atgr <atgr@PCBCN1323.gft.com>
2015-08-19 16:38:12 +0200

    Modification of StopDAOImplTest

--- test/com/river/persistence/dao/implementation/StopDAOImplTest.java
+++ test/com/river/persistence/dao/implementation/StopDAOImplTest.java
@@ -11,12 +11,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.river.entity.Address;
 import com.river.entity.Stop;
 
-
-
 public class StopDAOImplTest {
-	
-	ApplicationContext appContext = new ClassPathXmlApplicationContext(
-			"config/spring-config.xml");
+
+	ApplicationContext appContext = new ClassPathXmlApplicationContext("config/spring-config.xml");
 	Stop stopNull;
 	Stop stopGood;
 	Stop stopNotExists;
@@ -28,110 +25,110 @@ public class StopDAOImplTest {
 	public void deleteInserted(Stop toDelete) {
 		dao.delete(toDelete);
 	}
-	
+
 	@Before
-	public void init(){
-		addressExists = new Address(1,0,"patica","pata");
-		addressNotExists = new Address(2,0,"bicicleta","bike");
-		stopNull = new Stop(null,null);
+	public void init() {
+		addressExists = new Address(1, 0, "patica", "pata");
+		addressNotExists = new Address(2, 0, "bicicleta", "bike");
+		stopNull = new Stop(null, null);
 		stopGood = new Stop(addressExists);
 		stopNotExists = new Stop(addressNotExists);
 	}
-		
+
 	@Test
-	public void createStopWithNullValues(){
+	public void createStopWithNullValues() {
 		toUse = dao.create(stopNull);
 		Assert.assertNull("Stop must be null", toUse);
-		if(toUse != null){
+		if (toUse != null) {
 			deleteInserted(toUse);
 		}
 	}
-	
+
 	@Test
 	public void createStopWithGoodValues() {
 		toUse = dao.create(stopGood);
 		Assert.assertNotNull("Stop must be not null", toUse);
-		if(toUse != null){
+		if (toUse != null) {
 			deleteInserted(toUse);
 		}
 	}
-	
+
 	@Test
-	public void readStopThatExists(){
+	public void readStopThatExists() {
 		dao.create(stopGood);
 		toUse = dao.read(stopGood);
 		Assert.assertNotNull("Stop must be not null", toUse);
-		if(toUse != null){
+		if (toUse != null) {
 			deleteInserted(toUse);
 		}
 	}
-	
+
 	@Test
-	public void readStopNull(){
+	public void readStopNull() {
 		toUse = dao.read(stopNull);
 		Assert.assertNull("Stop must be null", toUse);
 	}
-	
+
 	@Test
-	public void readStopThatNotExists(){
+	public void readStopThatNotExists() {
 		toUse = dao.read(stopNotExists);
 		Assert.assertNull("Stop must be null", toUse);
 	}
-	
+
 	@Test
-	public void deleteStopThatExists(){
+	public void deleteStopThatExists() {
 		dao.create(stopGood);
 		toUse = dao.delete(stopGood);
 		Assert.assertNotNull("Stop must be not null", toUse);
-		if(toUse != null){
+		if (toUse != null) {
 			deleteInserted(toUse);
 		}
 	}
-	
+
 	@Test
-	public void deleteStopThatNotExists(){
+	public void deleteStopThatNotExists() {
 		toUse = dao.delete(stopNotExists);
 		Assert.assertNull("Stop must be null", toUse);
 	}
-	
+
 	@Test
-	public void updateStopThatExists(){
+	public void updateStopThatExists() {
 		dao.create(stopGood);
 		addressExists.setRoadType("COCHE");
 		stopGood.setAdress(addressExists);
 		toUse = dao.update(stopGood);
 		Assert.assertNotNull("Stop must be not null", toUse);
-		if(toUse != null){
+		if (toUse != null) {
 			deleteInserted(toUse);
 		}
 	}
-	
+
 	@Test
-	public void updateStopThatNotExists(){
+	public void updateStopThatNotExists() {
 		toUse = dao.update(stopNotExists);
 		Assert.assertNull("Stop must be null", toUse);
 	}
-	
+
 	@Test
-	public void updateStopNull(){
+	public void updateStopNull() {
 		toUse = dao.update(stopNull);
 		Assert.assertNull("Stop must be null", toUse);
 	}
-	
+
 	@Test
-	public void readListCorrectly(){
+	public void readListCorrectly() {
 		dao.create(stopGood);
 		List<Stop> stops = dao.read();
-		Assert.assertTrue("list must be full",!stops.isEmpty());
-		for(Stop p: stops){
-				deleteInserted(p);
+		Assert.assertTrue("list must be full", !stops.isEmpty());
+		for (Stop p : stops) {
+			deleteInserted(p);
 		}
 	}
-	
+
 	@Test
-	public void readListNotCorrectly(){
+	public void readListNotCorrectly() {
 		List<Stop> stops = dao.read();
-		Assert.assertTrue("list must be empty",stops.isEmpty());
+		Assert.assertTrue("list must be empty", stops.isEmpty());
 	}
 
 }

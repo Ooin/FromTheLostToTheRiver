08fa913b2794d65c1016258a633a5fea423d7e75
arvd <arvd@PCBCN1084.gft.com>
2015-08-21 09:34:38 +0200

    rafter facade ?

---
+++ src/com/river/business/facade/Rafter.java
@@ -0,0 +1,15 @@
+package com.river.business.facade;
+
+public class Rafter {
+	/**
+	 * As a user i want to create my rafter
+	 */
+	
+	/**
+	 * As a rafter i want to edit my profile
+	 */
+	
+	/**
+	 * As a rafter i want to close my account
+	 */
+}
